666fad33d201448b8455271d64cdfb7779082f81
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-15 17:12:19 +0200

    refactor: changes in roleEntity for logging times

--- src/domain/models/roleEntity.go
+++ src/domain/models/roleEntity.go
@@ -1,13 +1,21 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type RoleEntity struct {
-	ID         uuid.UUID `json:"id"`
-	RoleType   string    `json:"roleType"`
-	Permission int       `json:"permission"`
-	AdminRole  bool      `json:"adminRole"`
-	ModRole    bool      `json:"modRole"`
+	ID         uuid.UUID      `json:"id"`
+	RoleType   string         `json:"roleType"`
+	Permission int            `json:"permission"`
+	AdminRole  bool           `json:"adminRole"`
+	ModRole    bool           `json:"modRole"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt  gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
 
 func (RoleEntity) TableName() string {
