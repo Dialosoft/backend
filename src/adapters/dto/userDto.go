834288965311d350984586344c6e60d364c5d49d
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-15 17:11:35 +0200

    feat: user and role dto addeds

--- src/adapters/dto/userDto.go
+++ src/adapters/dto/userDto.go
@@ -1,7 +1,21 @@
 package dto
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type UserDto struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	ID        uuid.UUID      `json:"id"`
+	Username  string         `json:"username"`
+	Password  string         `json:"password"`
+	Email     string         `json:"email"`
+	Locked    bool           `json:"locked"`
+	Disable   bool           `json:"disable"`
+	Role      RoleDto        `json:"role"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt"`
 }
