e797ff9a4fa963c6b784121bf44d32636f46056b
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-15 17:12:38 +0200

    feat: error package added

---
+++ src/pkg/errorsUtils/errors.go
@@ -0,0 +1,9 @@
+// Utility package for error management in program execution due to errors that Dialosoft manages internally
+package errorsUtils
+
+import "errors"
+
+var (
+	// Error variable for when one of the parameters is null or empty
+	ErrParameterCannotBeNull = errors.New("no parameter can be null")
+)
