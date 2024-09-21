428945783bf72c2384a671662f7c8929767c4927
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-21 12:20:16 +0200

    feat: categoryService and forum added

---
+++ src/domain/services/categoryService.go
@@ -0,0 +1,122 @@
+package services
+
+import (
+	"github.com/Dialosoft/src/adapters/dto"
+	"github.com/Dialosoft/src/adapters/mapper"
+	"github.com/Dialosoft/src/adapters/repository"
+	"github.com/Dialosoft/src/domain/models"
+	"github.com/google/uuid"
+)
+
+type CategoryService interface {
+	GetAllCategories() ([]*dto.CategoryDto, error)
+	GetCategoryByID(id uuid.UUID) (*dto.CategoryDto, error)
+	GetCategoryByName(name string) (*dto.CategoryDto, error)
+	CreateCategory(categoryDto dto.CategoryDto) (uuid.UUID, error)
+	UpdateCategory(id uuid.UUID, name string, description string) error
+	DeleteCategory(id uuid.UUID) error
+	RestoreCategory(id uuid.UUID) error
+}
+
+type categoryServiceImpl struct {
+	categoryRepository repository.CategoryRepository
+}
+
+// CreateCategory implements CategoryService.
+func (service *categoryServiceImpl) CreateCategory(categoryDto dto.CategoryDto) (uuid.UUID, error) {
+	newCategory := models.Category{
+		Name:        categoryDto.Name,
+		Description: categoryDto.Description,
+	}
+
+	id, err := service.categoryRepository.Create(newCategory)
+	if err != nil {
+		return uuid.UUID{}, nil
+	}
+
+	return id, nil
+}
+
+// DeleteCategory implements CategoryService.
+func (service *categoryServiceImpl) DeleteCategory(id uuid.UUID) error {
+	err := service.categoryRepository.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetAllCategories implements CategoryService.
+func (service *categoryServiceImpl) GetAllCategories() ([]*dto.CategoryDto, error) {
+	var categoriesDtos []*dto.CategoryDto
+
+	categoriesEntities, err := service.categoryRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range categoriesEntities {
+		categoryDto := mapper.CategoryEntityToCategoryDto(v)
+		categoriesDtos = append(categoriesDtos, categoryDto)
+	}
+
+	return categoriesDtos, err
+}
+
+// GetCategoryByID implements CategoryService.
+func (service *categoryServiceImpl) GetCategoryByID(id uuid.UUID) (*dto.CategoryDto, error) {
+	categoryEntity, err := service.categoryRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return mapper.CategoryEntityToCategoryDto(categoryEntity), nil
+}
+
+// GetCategoryByName implements CategoryService.
+func (service *categoryServiceImpl) GetCategoryByName(name string) (*dto.CategoryDto, error) {
+	categoryEntity, err := service.categoryRepository.FindByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return mapper.CategoryEntityToCategoryDto(categoryEntity), nil
+}
+
+// RestoreCategory implements CategoryService.
+func (service *categoryServiceImpl) RestoreCategory(id uuid.UUID) error {
+	err := service.categoryRepository.Restore(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateCategoryDescription implements CategoryService.
+func (service *categoryServiceImpl) UpdateCategory(id uuid.UUID, name string, description string) error {
+	category, err := service.categoryRepository.FindByID(id)
+	if err != nil {
+		return err
+	}
+
+	if name != "" {
+		category.Name = name
+	}
+
+	if description != "" {
+		category.Description = description
+	}
+
+	err = service.categoryRepository.Update(*category)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewCategoryService(categoryRepository repository.CategoryRepository) CategoryService {
+	return &categoryServiceImpl{categoryRepository: categoryRepository}
+}
---
+++ src/domain/services/forumService.go
@@ -0,0 +1,131 @@
+package services
+
+import (
+	"github.com/Dialosoft/src/adapters/dto"
+	"github.com/Dialosoft/src/adapters/mapper"
+	"github.com/Dialosoft/src/adapters/repository"
+	"github.com/Dialosoft/src/domain/models"
+	"github.com/google/uuid"
+)
+
+type ForumService interface {
+	GetAllForums() ([]*models.Forum, error)
+	GetForumByID(id uuid.UUID) (*models.Forum, error)
+	GetForumByName(name string) (*models.Forum, error)
+	CreateForum(forumDto dto.ForumDto) error
+	UpdateForum(id uuid.UUID, forumDto dto.ForumDto) error
+	DeleteForum(id uuid.UUID) error
+	RestoreForum(id uuid.UUID) error
+}
+
+type forumServiceImpl struct {
+	forumRepository repository.ForumRepository
+}
+
+// CreateForum implements ForumService.
+func (service *forumServiceImpl) CreateForum(forumDto dto.ForumDto) error {
+	forumEntity := mapper.ForumDtoToForumEntity(&forumDto)
+
+	err := service.forumRepository.Create(*forumEntity)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteForum implements ForumService.
+func (service *forumServiceImpl) DeleteForum(id uuid.UUID) error {
+	err := service.forumRepository.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetAllForums implements ForumService.
+func (service *forumServiceImpl) GetAllForums() ([]*models.Forum, error) {
+	forums, err := service.forumRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return forums, nil
+}
+
+// GetForumByID implements ForumService.
+func (service *forumServiceImpl) GetForumByID(id uuid.UUID) (*models.Forum, error) {
+	forum, err := service.forumRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return forum, nil
+}
+
+// GetForumByName implements ForumService.
+func (service *forumServiceImpl) GetForumByName(name string) (*models.Forum, error) {
+	forum, err := service.forumRepository.FindByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return forum, nil
+}
+
+// RestoreForum implements ForumService.
+func (service *forumServiceImpl) RestoreForum(id uuid.UUID) error {
+	forum, err := service.forumRepository.FindByID(id)
+	if err != nil {
+		return err
+	}
+
+	if err = service.forumRepository.Restore(forum.ID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateForum implements ForumService.
+func (service *forumServiceImpl) UpdateForum(id uuid.UUID, forumDto dto.ForumDto) error {
+	forum, err := service.forumRepository.FindByID(id)
+	if err != nil {
+		return err
+	}
+
+	{
+		if forumDto.Name != "" {
+			forum.Name = forumDto.Name
+		}
+
+		if forumDto.Description != "" {
+			forum.Description = forumDto.Description
+		}
+
+		forum.IsActive = forumDto.IsActive
+
+		if forumDto.Type != "" {
+			forum.Type = forumDto.Type
+		}
+
+		if forumDto.PostCount != 0 {
+			forum.PostCount = forumDto.PostCount
+		}
+
+		if forumDto.CategoryID != "" {
+			forum.CategoryID = forumDto.CategoryID
+		}
+	}
+
+	err = service.forumRepository.Update(*forum)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewForumService(forumRepository repository.ForumRepository) ForumService {
+	return &forumServiceImpl{forumRepository: forumRepository}
+}
