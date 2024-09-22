71c7adcda2327f9d61b6fe745a5dfdccc6913cf0
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-22 17:03:07 +0200

    feat: added newUser with pointer fields

--- src/adapters/http/request/userRequest.go
+++ src/adapters/http/request/userRequest.go
@@ -10,3 +10,10 @@ type UpdateUserRequest struct {
 	Username string `jso:"username"`
 	Email    string `json:"email"`
 }
+
+type NewUser struct {
+	Username *string `json:"username"`
+	Locked   *bool   `json:"locked"`
+	Disable  *bool   `json:"disable"`
+	RoleID   *string `json:"userID"`
+}
