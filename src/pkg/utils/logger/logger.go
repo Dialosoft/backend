940bf77204c05803c0e1eaae36404fe0ffd511bf
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-23 21:43:49 +0200

    refactor: logger improved

--- src/pkg/utils/logger/logger.go
+++ src/pkg/utils/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"io"
 	"os"
+	"runtime"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -24,10 +25,49 @@ func InitLogger() {
 	logger.SetLevel(logrus.InfoLevel)
 }
 
-func Info(msg string) {
-	logger.Info(msg)
+// Info logs an informational message.
+func Info(msg string, fields map[string]interface{}) {
+	logger.WithFields(logrus.Fields(fields)).Info(msg)
 }
 
-func Error(msg string) {
-	logger.Error(msg)
+// Warn logs a warning message.
+func Warn(msg string, fields map[string]interface{}) {
+	logger.WithFields(logrus.Fields(fields)).Warn(msg)
+}
+
+// Error logs an error message, including context such as file and line number.
+func Error(msg string, fields map[string]interface{}) {
+	// Capture the caller function, file, and line number
+	if pc, file, line, ok := runtime.Caller(1); ok {
+		fn := runtime.FuncForPC(pc)
+		fields["file"] = file
+		fields["line"] = line
+		fields["function"] = fn.Name()
+	}
+	logger.WithFields(logrus.Fields(fields)).Error(msg)
+}
+
+// Debug logs a debug message, typically used for low-level system information.
+func Debug(msg string, fields map[string]interface{}) {
+	logger.WithFields(logrus.Fields(fields)).Debug(msg)
+}
+
+// Fatal logs a fatal error message and exits the application.
+func Fatal(msg string, fields map[string]interface{}) {
+	// Capture caller information before exiting
+	if pc, file, line, ok := runtime.Caller(1); ok {
+		fn := runtime.FuncForPC(pc)
+		fields["file"] = file
+		fields["line"] = line
+		fields["function"] = fn.Name()
+	}
+	logger.WithFields(logrus.Fields(fields)).Fatal(msg)
+}
+
+// CaptureError logs an error message with an additional error object.
+func CaptureError(err error, msg string, fields map[string]interface{}) {
+	if err != nil {
+		fields["error"] = err.Error()
+		Error(msg, fields)
+	}
 }
