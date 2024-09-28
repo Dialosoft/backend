50e54ff20faabee8f30e95813bfdd77855805814
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-28 20:47:28 +0200

    feat: postlikes and post repository added

---
+++ src/adapters/repository/postLikesRepository.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"github.com/Dialosoft/src/domain/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type PostLikesRepository interface {
+	FindAllByPostID(postID uuid.UUID) ([]*models.PostLikes, error)
+	FindAllByUserIDAndPostID(postID uuid.UUID, userID uuid.UUID) ([]*models.PostLikes, error)
+	Save(postID uuid.UUID, userID uuid.UUID) error
+	Remove(postID uuid.UUID, userID uuid.UUID) error
+}
+
+type postLikesRepositoryImpl struct {
+	db *gorm.DB
+}
+
+// FindAll implements PostLikesRepository.
+func (repo *postLikesRepositoryImpl) FindAllByPostID(postID uuid.UUID) ([]*models.PostLikes, error) {
+	var postLikes []*models.PostLikes
+	result := repo.db.Find(&postLikes, "post_id = ?", postID)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return postLikes, nil
+}
+
+// FindAllByUserID implements PostLikesRepository.
+func (repo *postLikesRepositoryImpl) FindAllByUserIDAndPostID(postID uuid.UUID, userID uuid.UUID) ([]*models.PostLikes, error) {
+	var postLikes []*models.PostLikes
+	if err := repo.db.Find(&postLikes, "post_id = ? AND user_id = ?", postID, userID).Error; err != nil {
+		return nil, err
+	}
+	return postLikes, nil
+}
+
+// Save implements PostLikesRepository.
+func (repo *postLikesRepositoryImpl) Save(postID uuid.UUID, userID uuid.UUID) error {
+	if err := repo.db.Create(&models.PostLikes{
+		PostID: postID,
+		UserID: userID,
+	}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// Remove implements PostLikesRepository.
+func (repo *postLikesRepositoryImpl) Remove(postID uuid.UUID, userID uuid.UUID) error {
+	return repo.db.Delete(models.PostLikes{}, "post_id = ? AND user_id = ?", postID, userID).Error
+}
+
+func NewPostLikesRepository(db *gorm.DB) PostLikesRepository {
+	return &postLikesRepositoryImpl{db: db}
+}
--- src/adapters/repository/postRepository.go
+++ src/adapters/repository/postRepository.go
@@ -1,1 +1,112 @@
 package repository
+
+import (
+	"github.com/Dialosoft/src/domain/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type PostRepository interface {
+	FindAll() ([]*models.Post, error)
+	FindByID(ID uuid.UUID) (*models.Post, error)
+	FindByUserID(userID uuid.UUID) ([]*models.Post, error)
+	GetLikeCount(postID uuid.UUID) (int64, error)
+	Create(post models.Post) (*models.Post, error)
+	Update(postID uuid.UUID, updatedPost models.Post) error
+	Delete(postID uuid.UUID) error
+	Restore(postID uuid.UUID) error
+}
+
+type postRepositoryImpl struct {
+	db *gorm.DB
+}
+
+// FindAll implements PostRepository.
+func (repo *postRepositoryImpl) FindAll() ([]*models.Post, error) {
+	var posts []*models.Post
+	if err := repo.db.Preload("users").Find(&posts).Error; err != nil {
+		return nil, err
+	}
+	if len(posts) == 0 {
+		return nil, gorm.ErrRecordNotFound
+	}
+
+	return posts, nil
+}
+
+// FindByID implements PostRepository.
+func (repo *postRepositoryImpl) FindByID(ID uuid.UUID) (*models.Post, error) {
+	var post models.Post
+	if err := repo.db.Preload("users").Where("id = ?", ID.String()).First(&post).Error; err != nil {
+		return nil, err
+	}
+
+	return &post, nil
+}
+
+// FindByUserID implements PostRepository.
+func (repo *postRepositoryImpl) FindByUserID(userID uuid.UUID) ([]*models.Post, error) {
+	var posts []*models.Post
+	if err := repo.db.Preload("users").Where("user_id = ?", userID.String()).Find(&posts).Error; err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+// GetLikeCount implements PostRepository.
+func (repo *postRepositoryImpl) GetLikeCount(postID uuid.UUID) (int64, error) {
+	var likesCount int64
+	if err := repo.db.Model(models.PostLikes{}).Where("post_id = ?", postID).Count(&likesCount).Error; err != nil {
+		return 0, err
+	}
+
+	return likesCount, nil
+}
+
+// Create implements PostRepository.
+func (repo *postRepositoryImpl) Create(post models.Post) (*models.Post, error) {
+	result := repo.db.Save(&post)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &post, nil
+}
+
+// Update implements PostRepository.
+func (repo *postRepositoryImpl) Update(postID uuid.UUID, updatedPost models.Post) error {
+	result := repo.db.Model(&models.Post{}).
+		Where("id = ?", postID).
+		Updates(updatedPost)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	return nil
+}
+
+// Delete implements PostRepository.
+func (repo *postRepositoryImpl) Delete(postID uuid.UUID) error {
+	return repo.db.Delete(&models.Post{}, postID.String()).Error
+}
+
+// Restore implements PostRepository.
+func (repo *postRepositoryImpl) Restore(postID uuid.UUID) error {
+	result := repo.db.Unscoped().
+		Model(&models.Post{}).
+		Where("id = ?", postID).
+		Update("deleted_at", nil)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func NewPostRepository(db *gorm.DB) PostRepository {
+	return &postRepositoryImpl{db: db}
+}
