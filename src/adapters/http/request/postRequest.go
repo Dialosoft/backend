9544ffb147e03da1575130b71584a47549b2acf5
Flussen <53578660+Flussen@users.noreply.github.com>
2024-09-29 12:24:39 +0200

    feat: requests added

--- src/adapters/http/request/postRequest.go
+++ src/adapters/http/request/postRequest.go
@@ -1,9 +1,22 @@
 package request
 
 type NewPost struct {
-	UserID   string `json:"userID"`
-	Title    string `json:"title"`
-	Content  string `json:"content"`
-	Views    uint32 `json:"views"`
-	Comments uint32 `json:"comments"`
+	UserID  string `json:"userID"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type UpdatePostTitle struct {
+	Title  string `json:"title"`
+	PostID string `json:"postID"`
+}
+
+type UpdatePostContent struct {
+	Content string `json:"content"`
+	PostID  string `json:"postID"`
+}
+
+type LikeOrUnlikePost struct {
+	PostID string `json:"postID"`
+	UserID string `json:"userID"`
 }
