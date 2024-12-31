ffc072243ca04b6e6c503685cde45faf8e8d6cb3
matdevcoder <mat.enterprice@gmail.com>
2024-12-31 19:30:40 +0100

    test(domain/service): :white_check_mark: Add mockAbstractRepository to make DRY over crud method mocks

---
+++ src/adapters/repository/mockAbstractRepository.go
@@ -0,0 +1,61 @@
+package repository
+
+import (
+	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
+)
+
+// MockAbstractRepository is a mock implementation of the AbstractRepository interface
+type MockAbstractRepository[T Identifiable[K], K ID] struct {
+	mock.Mock
+}
+
+func (m *MockAbstractRepository[T, K]) FindAll() ([]T, error) {
+	args := m.Called()
+	return args.Get(0).([]T), args.Error(1)
+}
+
+func (m *MockAbstractRepository[T, K]) FindByID(id K) (T, error) {
+	args := m.Called(id)
+	return args.Get(0).(T), args.Error(1)
+}
+
+func (m *MockAbstractRepository[T, K]) FirstByKey(key, value string) (T, error) {
+	args := m.Called(key, value)
+	return args.Get(0).(T), args.Error(1)
+}
+
+func (m *MockAbstractRepository[T, K]) FindAllByKey(key, value string) ([]T, error) {
+	args := m.Called(key, value)
+	return args.Get(0).([]T), args.Error(1)
+}
+
+func (m *MockAbstractRepository[T, K]) Create(tx *gorm.DB, newEntity T) (T, error) {
+	args := m.Called(tx, newEntity)
+	return args.Get(0).(T), args.Error(1)
+}
+
+func (m *MockAbstractRepository[T, K]) Update(tx *gorm.DB, id K, newEntity T) error {
+	args := m.Called(tx, id, newEntity)
+	return args.Error(0)
+}
+
+func (m *MockAbstractRepository[T, K]) Delete(tx *gorm.DB, id K) error {
+	args := m.Called(tx, id)
+	return args.Error(0)
+}
+
+func (m *MockAbstractRepository[T, K]) Restore(tx *gorm.DB, id K) error {
+	args := m.Called(tx, id)
+	return args.Error(0)
+}
+
+func (m *MockAbstractRepository[T, K]) GetPreloads() []string {
+	args := m.Called()
+	return args.Get(0).([]string)
+}
+
+func (m *MockAbstractRepository[T, K]) GetType() string {
+	args := m.Called()
+	return args.String(0)
+}
